78f93de7da69cd40577e9366eb35a5af80c55708
Rod Knowlton <rod.knowlton@summitesp.com>
2021-12-20 15:49:08 -0600

    host adjustments for new salt server

--- home/bin/salt_to_ssh.py
+++ home/bin/salt_to_ssh.py
@@ -2,12 +2,13 @@
 import json
 import subprocess
 
+SALTMASTER_IP = "10.8.4.70"
 args = [
     "ssh",
-    "10.8.0.6",
+    SALTMASTER_IP,
     "sudo",
     "salt",
-    "saltmaster-3",
+    "saltmaster-01",
     "mine.get",
     "'*'",
     "vpn_ip",
@@ -16,7 +17,7 @@ args = [
 ]
 completed = subprocess.run(args, stdout=subprocess.PIPE)
 salt_out = json.loads(completed.stdout)
-hosts = salt_out["saltmaster-3"]
+hosts = salt_out["saltmaster-01"]
 for k, v in hosts.items():
     try:
         print(f"Host {k}\n    HostName {v[0]}\n")
