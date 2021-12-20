b80fbd979849720fa96e5c482c03ff2903d331fe
Rod Knowlton <rod.knowlton@summitesp.com>
2021-12-20 15:46:20 -0600

    org layer updates

--- home/.emacs.d/private/rk-org/layers.el
+++ home/.emacs.d/private/rk-org/layers.el
@@ -1,11 +1,11 @@
 (configuration-layer/declare-layer '(org :variables
-                                        org-enable-github-support t
-                                        org-enable-org-journal-support t
                                         org-enable-bootstrap-support t
-                                        org-enable-reveal-js-support t
-                                        org-enable-jira-support t
                                         org-enable-github-support t
+                                        org-enable-jira-support t
                                         org-enable-notifications t
+                                        org-enable-org-brain-support t
+                                        org-enable-org-journal-support t
+                                        org-enable-reveal-js-support t
                                         org-start-notification-daemon-on-startup t
                                         ;; org-enable-roam-support t
                                         jiralib-url "https://summitesp.atlassian.net:443"))
