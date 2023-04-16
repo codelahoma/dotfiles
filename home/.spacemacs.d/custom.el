d85aace16c3ce06bd506ec2440dcda6ab51af1e7
Rod Knowlton <codelahoma@gmail.com>
2023-04-16 17:55:56 -0500

    move org roam onto more convenient keys

--- home/.spacemacs.d/custom.el
+++ home/.spacemacs.d/custom.el
@@ -10,18 +10,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((rk/auto-update-word-count-enabled . t)
-     (projectile-project-name . "org-files")
+   '((projectile-project-name . "org-files")
      (typescript-backend . tide)
      (typescript-backend . lsp)
      (javascript-backend . tide)
      (javascript-backend . tern)
-     (javascript-backend . lsp)))
- '(warning-suppress-types '((emacs) (emacs) (emacs) (comp))))
+     (javascript-backend . lsp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil))))
  '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
  '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
