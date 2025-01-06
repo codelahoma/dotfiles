3320f9bf9b7e0e69259ac1ea6a93aa6ed9dd3c24
Rod Knowlton <codelahoma@gmail.com>
2025-01-06 07:23:42 -0600

    home mods, partial cleanup of dotspacemacs.org

--- home/.emacs.d/private/rk-org/layers.el
+++ home/.emacs.d/private/rk-org/layers.el
@@ -1,11 +1,17 @@
 (configuration-layer/declare-layer '(org :variables
-                                        org-enable-bootstrap-support t
-                                        org-enable-github-support t
-                                        org-enable-jira-support t
-                                        org-enable-notifications t
-                                        org-enable-org-brain-support t
-                                        org-enable-org-journal-support t
-                                        org-enable-reveal-js-support t
-                                        org-start-notification-daemon-on-startup t
-                                        ;; org-enable-roam-support t
-                                        jiralib-url "https://summitesp.atlassian.net:443"))
+                                         org-enable-appear-support t
+                                         org-appear-autolinks nil
+                                         org-enable-bootstrap-support t
+                                         org-enable-org-contacts-support nil
+                                         org-enable-hugo-support t
+                                         org-enable-modern-support t
+                                         org-enable-jira-support t
+                                         org-enable-org-journal-support t
+                                         org-enable-notifications t
+                                         org-enable-reveal-js-support t
+                                         org-enable-roam-support t
+                                         org-enable-roam-ui t
+                                         org-enable-sticky-header t
+                                         org-enable-transclusion-support t
+                                         org-projectile-file "TODOs.org"
+                                         org-start-notification-daemon-on-startup t))
