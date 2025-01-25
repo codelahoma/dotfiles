5220d86867430b94e247f09e9f4d4e96b00a4771
Rod Knowlton <rod@atlasup.com>
2025-01-25 12:04:40 -0600

    work edits

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -8,6 +8,7 @@ hs.logger.defaultLogLevel = "info"
 
 hyper = {"alt","cmd","ctrl","shift"}
 magic = {"alt","cmd","ctrl"}
+meh = {"alt", "ctrl", "shift"}
 
 application = hs.application
 hotkey = hs.hotkey
@@ -19,6 +20,48 @@ machine = hs.host.localizedName()
 
 work_machines = {["codelahoma"] = true, ["codelahoma-kw"] = true, ["codelahoma-kw-m1"] = true, ["codelahoma-atlasup"] = true }
 home_machines = {["m1-mini"] = true}
+
+-- -- https://github.com/fikovnik/ShiftIt/wiki/The-Hammerspoon-Alternative
+-- -- https://github.com/derekwyatt/dotfiles/blob/master/hammerspoon-init.lua
+-- hs.window.animationDuration = 0
+-- units = {
+--   right30       = { x = 0.70, y = 0.00, w = 0.30, h = 1.00 },
+--   right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
+--   right70       = { x = 0.30, y = 0.00, w = 0.70, h = 1.00 },
+--   left70        = { x = 0.00, y = 0.00, w = 0.70, h = 1.00 },
+--   left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
+--   left30        = { x = 0.00, y = 0.00, w = 0.30, h = 1.00 },
+--   top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
+--   bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
+--   upright30     = { x = 0.70, y = 0.00, w = 0.30, h = 0.50 },
+--   botright30    = { x = 0.70, y = 0.50, w = 0.30, h = 0.50 },
+--   upleft70      = { x = 0.00, y = 0.00, w = 0.70, h = 0.50 },
+--   botleft70     = { x = 0.00, y = 0.50, w = 0.70, h = 0.50 },
+--   maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 }
+-- }
+
+-- -- hs.hotkey.bind(meh, 'l', function() hs.window.focusedWindow():move(units.right30,    nil, true) end)
+-- -- hs.hotkey.bind(meh, 'h', function() hs.window.focusedWindow():move(units.left70,     nil, true) end)
+-- hs.hotkey.bind(meh, 'l', function() hs.window.focusedWindow():move(units.right50,    nil, true) end)
+-- hs.hotkey.bind(meh, 'h', function() hs.window.focusedWindow():move(units.left50,     nil, true) end)
+-- hs.hotkey.bind(meh, 'k', function() hs.window.focusedWindow():move(units.top50,      nil, true) end)
+-- hs.hotkey.bind(meh, 'j', function() hs.window.focusedWindow():move(units.bot50,      nil, true) end)
+-- hs.hotkey.bind(meh, ']', function() hs.window.focusedWindow():move(units.upright30,  nil, true) end)
+-- hs.hotkey.bind(meh, '[', function() hs.window.focusedWindow():move(units.upleft70,   nil, true) end)
+-- hs.hotkey.bind(meh, ';', function() hs.window.focusedWindow():move(units.botleft70,  nil, true) end)
+-- hs.hotkey.bind(meh, "'", function() hs.window.focusedWindow():move(units.botright30, nil, true) end)
+-- hs.hotkey.bind(meh, 'm', function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
+
+-- -- https://stackoverflow.com/a/58662204
+-- hs.hotkey.bind(meh, 'n', function()
+--     -- get the focused window
+--     local win = hs.window.focusedWindow()
+--     -- get the screen where the focused window is displayed, a.k.a. current screen
+--     local screen = win:screen()
+--     -- compute the unitRect of the focused window relative to the current screen
+--     -- and move the window to the next screen setting the same unitRect
+--     win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+-- end)
 
 hs.loadSpoon("SpoonInstall")
 
@@ -243,6 +286,7 @@ end)
 if work_machines[machine] ~= nil then
   hotkey.bind(hyper, "a", appLauncher('Arduino IDE'))
   hotkey.bind(hyper, "b", appLauncher('ChatGPT'))
+  hotkey.bind(magic, "b", appLauncher('Bazecor'))
   hotkey.bind(hyper, "c", hs.toggleConsole)
   hotkey.bind(magic, "c", appLauncher('Claude'))
   hotkey.bind(hyper, "d", appLauncher('Dash'))
